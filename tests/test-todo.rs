f762b4bc4d54b73b72d1daa3244e22cbdbbbdf51
jasenfinch <3043780+jasenfinch@users.noreply.github.com>
2026-02-01 21:58:25 +0000

    test: add integration tests

---
+++ tests/test-todo.rs
@@ -0,0 +1,25 @@
+use assert_cmd::cargo;
+use assert_cmd::Command;
+use tempfile::TempDir;
+
+#[test]
+fn test_todo() {
+    let temp_dir = TempDir::new().unwrap();
+    let temp_path = temp_dir.path();
+
+    let mut cmd = Command::new(cargo::cargo_bin!());
+    cmd.arg("-p").arg(temp_path).arg("list").assert().success();
+
+    let mut cmd = Command::new(cargo::cargo_bin!());
+    cmd.arg("-p")
+        .arg(temp_path)
+        .args(["add", "test", "-d", "a test task", "-t", "work,project"])
+        .assert()
+        .success();
+
+    let mut cmd = Command::new(cargo::cargo_bin!());
+    cmd.arg("-p").arg(temp_path).arg("list").assert().success();
+
+    let mut cmd = Command::new(cargo::cargo_bin!());
+    cmd.arg("-p").arg(temp_path).arg("clear").assert().success();
+}
