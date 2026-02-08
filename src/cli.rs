c31194db5786f99d6e86f9b6f84dd06fcde11f1f
jasenfinch <3043780+jasenfinch@users.noreply.github.com>
2026-02-08 18:58:50 +0000

    refactor: move description to Cargo.toml

--- src/cli.rs
+++ src/cli.rs
@@ -5,7 +5,7 @@ use crate::display;
 
 #[derive(Debug, Parser)]
 #[command(name = "todo")]
-#[command(author,version,about = "A task management and productivity CLI tool", long_about = None)]
+#[command(author,version, long_about = None)]
 #[command(after_help = r#"EXAMPLES:
     todo add "Fix bug" --diff 5 --deadline friday --tags work,urgent
     todo list --view minimal
